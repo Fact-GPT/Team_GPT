658566cdb12e646e1ec216f960f60e14bff8dd40
Melissa Zhu <zhum17@cardiff.ac.uk>
2023-04-17 14:27:11 +0100

    More updates, trying to add DataCommons API call

--- GPT_Flask/blog/app.py
+++ GPT_Flask/blog/app.py
@@ -35,7 +35,6 @@ def gpt_request(query):
     }
     data = {
         "model": "gpt-4",
-        # We don't need system message cos we don't ask fact checking
         "messages" : [{"role": "user", "content": query}],
         "max_tokens": 2000,
         "temperature": 0
@@ -81,14 +80,147 @@ def process_text():
     for query in modified_queries:
         response = gpt_request(query)
         optimised_queries.append(response)
-    
+
+    processed_data = process_claims(claims)
+
     output = {
-        "original_text": text,
-        "claims": claims,
-        "fact_check_reviews": summaries
-    }
+            "original_text": text,
+            "claims": claims,
+            "highlighted_text": highlighted_text
+            "fact_check_reviews": summaries
+        }
     
     return jsonify(output)
+
+def process_claims(optimised_queries): 
+    my_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36; Melissa Zhu/ZhuM17@cardiff.ac.uk. Working on data for class.'}
+
+    endpoint = 'https://factchecktools.googleapis.com/v1alpha1/claims:search'
+
+    links = []
+
+    for optimised_query in optimised_queries:
+        
+        query = optimised_query
+        language = 'en'
+        max_days = 10000 #Max age of returned search results, in days
+        page_size = 20 #Number of pages in the search results
+
+        reviewPublisherSiteFilter = '' #Filter by review publisher (can be blank)
+
+        api_key = 'AIzaSyDKpe6j4lqR_yuy6FZMI3NEdY9VP7Fa2jI' # Melissa's key
+
+        url = f'{endpoint}?query={query}&key={api_key}&languageCode={language}&maxAgeDays={max_days}&pageSize={page_size}&reviewPublisherSiteFilter={reviewPublisherSiteFilter}'
+        links.append(url)
+
+        results = []
+        for link in links:
+            req = requests.get(link, headers = my_headers)
+            data = req.json()
+            if 'claims' in data: 
+                claims = data['claims']
+                results.append(claims)
+                
+        for result in results: 
+            if len(result) > 3:  #Limit to max 3 results/query for brevity
+                result = result[:3]
+
+        search_queries = []
+        claims = []
+        originators = []
+        claim_dates = []
+        reviews = []
+        publisher_names = []
+        publisher_sites = []
+        review_urls = []
+        titles = []
+        review_dates = []
+        ratings = []
+
+        for i in range(len(results)): 
+            search_query = optimised_queries[i] #Match queries to results
+            search_queries += len(results[i]) * [search_query]
+
+        for result in results: 
+            for i in range(len(result)):
+                claim = result[i]['text']
+                claims.append(claim)
+
+        for result in results: 
+            for i in range(len(result)):
+                if 'claimant' in result[i]:
+                    originator = result[i]['claimant']
+                    originators.append(originator)
+                else: 
+                    originators.append('NA')
+
+        for result in results: 
+            for i in range(len(result)):
+                if 'claimDate' in result[i]:
+                    date = result[i]['claimDate']
+                    claim_dates.append(date)
+                else: 
+                    claim_dates.append('NA')
+
+        for result in results: 
+            for i in range(len(result)):
+                first_review = result[i]['claimReview'][0]
+                reviews.append(first_review) #Most only have one review but some of them have many reviews all saying the same thing so gonna keep it to one to standardise the dataframe
+
+        for review in reviews: 
+            if 'publisher' in review and 'name' in review['publisher']: 
+                publisher_name = review['publisher']['name']
+                publisher_names.append(publisher_name)
+            else: 
+                publisher_names.append('NA')
+
+        for review in reviews: 
+            if 'publisher' in review and 'site' in review['publisher']: 
+                publisher_site = review['publisher']['site']
+                publisher_sites.append(publisher_site)
+            else: 
+                publisher_sites.append('NA')
+
+        for review in reviews: 
+            if 'url' in review: 
+                review_url = review['url']
+                review_urls.append(review_url)
+            else: 
+                review_urls.append('NA')
+                
+        for review in reviews: 
+            if 'title' in review: 
+                title = review['title']
+                titles.append(title)
+            else: 
+                titles.append('NA')
+
+        for review in reviews: 
+            if 'reviewDate' in review: 
+                review_date = review['reviewDate']
+                review_dates.append(review_date)
+            else: 
+                review_dates.append('NA')
+
+        for review in reviews: 
+            if 'textualRating' in review: 
+                rating = review['textualRating']
+                ratings.append(rating)
+            else: 
+                ratings.append('NA')
+
+        df = pd.DataFrame({'query': search_queries,
+                        'claim': claims, 
+                        'originator': originators, 
+                        'claim_date': claim_dates, 
+                        'review_publisher': publisher_names, 
+                        'publisher_site': publisher_sites, 
+                        'review_url': review_urls, 
+                        'review_title': titles, 
+                        'review_date': review_dates, 
+                        'verdict': ratings})
+        
+    return df
 
     if __name__ == "__main__":
         app.run()
